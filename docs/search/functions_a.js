ee7181acac3d7f86aeab0bd1ac9dd286e7d6658a
Shervin Nourbakhsh <shervin.nourbakhsh@ill.fr>
2020-10-23 12:32:02 +0200

    doc

---
+++ docs/search/functions_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['wavelength',['wavelength',['../classRay.html#a561d7716279f672d9d718c6ae18ded91',1,'Ray']]],
+  ['weight',['weight',['../classRay.html#a35c855b517234b1e69869e8298658cd0',1,'Ray']]]
+];
