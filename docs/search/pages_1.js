49f9d625539e385b4e871e0fb431030cbaa74814
Shervin Nourbakhsh <shervin.nourbakhsh@ill.fr>
2021-09-20 12:32:41 +0200

    updated documentation

--- docs/search/pages_1.js
+++ docs/search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['mainpage',['mainpage',['../md__users_nourbakhsh_panosc_openPMD_raytrace_API_doc_mainpage.html',1,'']]]
+  ['mainpage',['mainpage',['../md__opt_panosc_openPMD_extension_openPMD_raytrace_API_doc_mainpage.html',1,'']]]
 ];
